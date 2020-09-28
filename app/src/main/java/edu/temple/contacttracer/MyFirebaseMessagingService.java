808795c3daf14a5531d005664865a299cab22cce
Evan Fiordeliso <evan.fiordeliso@gmail.com>
2020-09-28 13:03:10 -0400

    Set up firebase

---
+++ app/src/main/java/edu/temple/contacttracer/MyFirebaseMessagingService.java
@@ -0,0 +1,23 @@
+package edu.temple.contacttracer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    public MyFirebaseMessagingService() {
+    }
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
+}
